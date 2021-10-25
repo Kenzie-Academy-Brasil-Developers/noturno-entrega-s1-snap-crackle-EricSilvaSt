634221a1440793e68ffecd9a192c3ede72f7d5e3
Eric Silva <54083779+EricSilvaSt@users.noreply.github.com>
2021-10-25 20:43:33 -0300

    Correções solicitadas da tarefa SnapCrackle
    Atualização da entraga 43 - SnapCrackle, com as correções solicitadas no retorno.

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,42 +1,40 @@
-//arrays, loops, condicionais e strings
-
-//Tarefa
-//ok -    Escreva uma função chamada snapCrackle que leva um parâmetro denominado: maxValue.
-//        Esta função deve fazer um loop de 1 até maxValue (inclusive) e montar uma string com as seguintes condições 
-//        para cada número:
-//        Se o número for ímpar, no lugar dele, concatenar "Snap" no final da string.
-//        Se o número for múltiplo de 5, no lugar dele, concatenar "Crackle" no final da string.
-//        Se o número for tanto ímpar quanto múltiplo de 5, no lugar dele, concatenar "SnapCrackle" no final da string.
-//        Se número não for nem ímpar e nem múltiplo de 5, concatenar o próprio número no final da string.
-//        Seus itens devem ser separados sempre por vírgula e espaço (veja o exemplo).
-//        Esta função deve retornar a string obtida.
+/* Tarefa
+   Escreva uma função chamada snapCrackle que leva um parâmetro denominado: maxValue.
+   Esta função deve fazer um loop de 1 até maxValue (inclusive) e montar uma string com as seguintes condições 
+   para cada número:
+   Se o número for ímpar, no lugar dele, concatenar "Snap" no final da string.
+   Se o número for múltiplo de 5, no lugar dele, concatenar "Crackle" no final da string.
+   Se o número for tanto ímpar quanto múltiplo de 5, no lugar dele, concatenar "SnapCrackle" no final da string.
+   Se número não for nem ímpar e nem múltiplo de 5, concatenar o próprio número no final da string.
+   Esta função deve retornar a string obtida.*/
 
 function snapCrackle(maxValue){
-    let texto= ""
-    for (let numero=1; numero <= maxValue; numero++){
+
+    let texto = ""
+    
+    for (let numero = 1; numero <= maxValue; numero++){
     
         if ((numero % 2 ==0) && (numero % 5 !==0)){
-        texto += numero +","
+        texto += numero
     }    
     
     if ((numero % 2 !==0) && (numero % 5 ==0)){
-        texto += " SnapCrackle, "
+        texto += "SnapCrackle"
     }
     
     else if (numero % 2 !==0){
-        texto += " Snap, "
+        texto += "Snap"
     }
     
     else if (numero % 5 ==0){
-        texto += " Crackle, "
+        texto += "Crackle"
     }
     
-    
-    
-    
+    if (numero < maxValue){
+      texto += ", "
+    }
 }
-console.log(texto)
+return texto
 }
-// retornar string obtida
 snapCrackle(15)
-
+// retornar string obtida
